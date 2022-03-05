c6a5540afd6a55d2b515f0391fb86f4cfaed8523
Shahbaaz <shahbaazc@gmail.com>
2022-03-05 21:28:43 +0530

    Open modal with useState

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,14 +2,14 @@ import { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../Assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton openModal={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A tables full of delicious fod!" />
       </div>
     </Fragment>
